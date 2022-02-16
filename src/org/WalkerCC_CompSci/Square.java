9938069ff3f01e9ace4bca382d1c172303ddab8e
solchtyl001 <88106057+solchtyl001@users.noreply.github.com>
2022-02-16 17:20:42 -0500

    Update Square.java

--- src/org/WalkerCC_CompSci/Square.java
+++ src/org/WalkerCC_CompSci/Square.java
@@ -1,9 +1,7 @@
-package org.WalkerCC_CompSci;
-
 public final class Square extends Rectangle{
 
     public Square(double sideLength) {
-        super();  //What goes here?
+        super(sideLength, sideLength);  
 
     }
 
@@ -12,7 +10,9 @@ public final class Square extends Rectangle{
     }
 
     public void setSideLength(double width){
-        //What goes here?
+        this.length = width;
+        this.width = width;
+        
     }
 
 
